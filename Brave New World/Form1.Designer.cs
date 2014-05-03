0a23e62369abd487365c07fe7db593649f726cb6
kmne68 <kmne68@gmail.com>
2014-05-03 14:43:19 -0500

    Adding form files.

---
+++ Brave New World/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Brave_New_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_keyOutput = new System.Windows.Forms.TextBox();
+            this.txt_biome1 = new System.Windows.Forms.TextBox();
+            this.txt_biome2 = new System.Windows.Forms.TextBox();
+            this.txt_biome3 = new System.Windows.Forms.TextBox();
+            this.txt_biome4 = new System.Windows.Forms.TextBox();
+            this.txt_biome5 = new System.Windows.Forms.TextBox();
+            this.txt_biome6 = new System.Windows.Forms.TextBox();
+            this.txt_biome7 = new System.Windows.Forms.TextBox();
+            this.txt_test1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "build key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_keyOutput
+            // 
+            this.txt_keyOutput.Location = new System.Drawing.Point(58, 12);
+            this.txt_keyOutput.Name = "txt_keyOutput";
+            this.txt_keyOutput.Size = new System.Drawing.Size(100, 20);
+            this.txt_keyOutput.TabIndex = 1;
+            // 
+            // txt_biome1
+            // 
+            this.txt_biome1.Location = new System.Drawing.Point(58, 38);
+            this.txt_biome1.Name = "txt_biome1";
+            this.txt_biome1.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome1.TabIndex = 2;
+            // 
+            // txt_biome2
+            // 
+            this.txt_biome2.Location = new System.Drawing.Point(58, 64);
+            this.txt_biome2.Name = "txt_biome2";
+            this.txt_biome2.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome2.TabIndex = 3;
+            // 
+            // txt_biome3
+            // 
+            this.txt_biome3.Location = new System.Drawing.Point(58, 90);
+            this.txt_biome3.Name = "txt_biome3";
+            this.txt_biome3.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome3.TabIndex = 4;
+            // 
+            // txt_biome4
+            // 
+            this.txt_biome4.Location = new System.Drawing.Point(58, 116);
+            this.txt_biome4.Name = "txt_biome4";
+            this.txt_biome4.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome4.TabIndex = 5;
+            // 
+            // txt_biome5
+            // 
+            this.txt_biome5.Location = new System.Drawing.Point(58, 142);
+            this.txt_biome5.Name = "txt_biome5";
+            this.txt_biome5.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome5.TabIndex = 6;
+            // 
+            // txt_biome6
+            // 
+            this.txt_biome6.Location = new System.Drawing.Point(58, 168);
+            this.txt_biome6.Name = "txt_biome6";
+            this.txt_biome6.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome6.TabIndex = 7;
+            // 
+            // txt_biome7
+            // 
+            this.txt_biome7.Location = new System.Drawing.Point(58, 194);
+            this.txt_biome7.Name = "txt_biome7";
+            this.txt_biome7.Size = new System.Drawing.Size(100, 20);
+            this.txt_biome7.TabIndex = 8;
+            // 
+            // txt_test1
+            // 
+            this.txt_test1.Location = new System.Drawing.Point(237, 13);
+            this.txt_test1.Name = "txt_test1";
+            this.txt_test1.Size = new System.Drawing.Size(100, 20);
+            this.txt_test1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 261);
+            this.Controls.Add(this.txt_test1);
+            this.Controls.Add(this.txt_biome7);
+            this.Controls.Add(this.txt_biome6);
+            this.Controls.Add(this.txt_biome5);
+            this.Controls.Add(this.txt_biome4);
+            this.Controls.Add(this.txt_biome3);
+            this.Controls.Add(this.txt_biome2);
+            this.Controls.Add(this.txt_biome1);
+            this.Controls.Add(this.txt_keyOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_keyOutput;
+        private System.Windows.Forms.TextBox txt_biome1;
+        private System.Windows.Forms.TextBox txt_biome2;
+        private System.Windows.Forms.TextBox txt_biome3;
+        private System.Windows.Forms.TextBox txt_biome4;
+        private System.Windows.Forms.TextBox txt_biome5;
+        private System.Windows.Forms.TextBox txt_biome6;
+        private System.Windows.Forms.TextBox txt_biome7;
+        private System.Windows.Forms.TextBox txt_test1;
+    }
+}
+
---
+++ Brave New World/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+using BiomeGeneration;
+
+namespace Brave_New_World
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ /*           Point[] points = {
+               new Point(10, 300),
+               new Point(60, 05),
+               new Point(75, 40),
+               new Point(10, 10),
+               new Point(40, 45),
+               new Point(10, 20),
+               new Point(15, 30),
+               new Point(200, 450)}; */      // keys of this last point
+
+            System.Windows.Point[] points = {
+               new System.Windows.Point(10, 300),
+               new System.Windows.Point(10, 300),
+               new System.Windows.Point(10, 300),
+               new System.Windows.Point(10, 10),
+               new System.Windows.Point(10, 10),
+               new System.Windows.Point(10, 10),
+               new System.Windows.Point(10, 10),
+               new System.Windows.Point(10, 300)};
+
+            Biome biome = new Biome(points, 16);
+
+            //txt_keyOutput.SelectedText = biome.GetCustomKey();
+
+            txt_keyOutput.SelectedText = biome.m_key[0];
+            txt_biome1.SelectedText = biome.m_key[1];
+            txt_biome2.SelectedText = biome.m_key[2];
+            txt_biome3.SelectedText = biome.m_key[3];
+            txt_biome4.SelectedText = biome.m_key[4];
+            txt_biome5.SelectedText = biome.m_key[5];
+            txt_biome6.SelectedText = biome.m_key[6];
+            txt_biome7.SelectedText = biome.m_key[7];
+            txt_test1.SelectedText = biome.FetchBiome(biome.m_key[0]);
+        
+        
+        }
+
+
+    }
+}
